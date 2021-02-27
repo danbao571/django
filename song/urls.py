d3c9cad4971c65dc0c19bdc87b21103baf6c1b46
danbao571 <1969236660@qq.com>
2021-02-27 12:50:59 +0800

    更新

--- song/urls.py
+++ song/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path
 from . import views
+from django.views.decorators.csrf import csrf_exempt
+
 
 app_name = 'song'
 urlpatterns = [
     path('', views.MusicView.as_view(), name='music'),
     path('get_song_url', views.get_song_url, name='get_song_url'),
     path('search_song', views.search_song, name='search_song'),
-    path('remove_bg', views.remove_bg, name='remove_bg'),
+    path('remove_bg', csrf_exempt(views.remove_bg), name='remove_bg'),
     path('download_music', views.download_music, name='download_music'),
 ]
