a2f4921b4de6174eb9d6b8fbbde2de94845b6d5a
danbao571 <1969236660@qq.com>
2021-02-28 11:20:40 +0800

    更新

--- info_site/settings.py
+++ info_site/settings.py
@@ -44,10 +44,8 @@ INSTALLED_APPS = [
     'user',
     'ckeditor',
     'ckeditor_uploader',
-    'corsheaders',
     'song',
     'blog',
-    'comment',
 ]
 
 MIDDLEWARE = [
@@ -207,30 +205,4 @@ CACHES = {
         'LOCATION': 'my_cache_table',
     }
 }
-CORS_ALLOW_CREDENTIALS = True
-CORS_ORIGIN_ALLOW_ALL = True
-CORS_ORIGIN_WHITELIST = ()
 
-CORS_ALLOW_METHODS = (
-    'DELETE',
-    'GET',
-    'OPTIONS',
-    'PATCH',
-    'POST',
-    'PUT',
-    'VIEW',
-)
-
-CORS_ALLOW_HEADERS = (
-    'XMLHttpRequest',
-    'X_FILENAME',
-    'accept-encoding',
-    'authorization',
-    'content-type',
-    'dnt',
-    'origin',
-    'user-agent',
-    'x-csrftoken',
-    'x-requested-with',
-    'Pragma',
-)
